e66c1e081b769641f9d7b8187f50f911660b7059
Moussa Ndour <mndour428@gmail.com>
2025-07-30 19:53:28 +0000

    formulaire complet

--- projet_restaurant/gestion_menus_plats/views.py
+++ projet_restaurant/gestion_menus_plats/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render,redirect
 from .models import Plat
 from .forms import PlatForm
+from django.contrib.auth.decorators import login_required
+from django.core.exceptions import PermissionDenied
 
 def plats(request):
     plats = Plat.objects.all()
@@ -11,12 +13,16 @@ def plats(request):
     ]
     return render(request, 'gestion_menus_plats/plats.html', {'plats': plats,'categories':categories})
 
+@login_required
 def ajouter_plat(request):
+    if request.user.role != 'personnel':
+        raise PermissionDenied("Accès réservé au personnel.")
+    
     if request.method == 'POST':
         form = PlatForm(request.POST, request.FILES)
         if form.is_valid():
             form.save()
-            return redirect('nom_de_ta_vue_de_liste')  # adapte selon ton projet
+            return redirect('nom_de_ta_vue_de_liste')
     else:
         form = PlatForm()
     return render(request, 'gestion_menus_plats/ajouter.html', {'form': form})
